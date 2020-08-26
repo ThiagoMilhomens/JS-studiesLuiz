5ebbc15a08c38fd5506b6c096bcccbfcd1524f93
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-08-26 01:11:21 -0300

    ago26

--- aula39-ExercícioCalculeIMC/assets/js/main.js
+++ aula39-ExercícioCalculeIMC/assets/js/main.js
@@ -8,8 +8,8 @@ function recebeEventoForm(evento) {
 
     evento.preventDefault(1)
 
-    const inputAltura = form.querySelector('#alturacont')
-    const inputPeso = form.querySelector('#pesocont')
+    const inputAltura = form.querySelector('#altura')
+    const inputPeso = form.querySelector('#peso')
 
     const altura = Number(inputAltura.value.replace(',', '.'))
     const peso = Number(inputPeso.value.replace(',', '.'))
@@ -28,7 +28,7 @@ function recebeEventoForm(evento) {
         } else if (imc < 18.5) {
             return `Seu IMC é ${imc} (Abaixo do peso)`
         } else if (imc >= 18.5 && imc <= 24.9) {
-            return `Seu IMC é ${imc} (Peso Normal)`
+            return `Seu IMC é ${imc} (Peso normal)`
         } else if (imc >= 25 && imc <= 29.9) {
             return `Seu IMC é ${imc} (Sobrepeso)`
         } else if (imc >= 30 && imc <= 34.9) {
