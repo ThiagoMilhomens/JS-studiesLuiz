8476b0d893b2c9ab0506a1febff3ecee76e4a650
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-08-27 00:44:42 -0300

    ago26

---
+++ teste/operaçãoTernária/ternario.js
@@ -0,0 +1,11 @@
+ const pontuacaoUsuario = 1000
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal'
+
+console.log(nivelUsuario(500))
+// Usuário VIP
+
+const corUsuario = null
+const corPadrao = corUsuario || 'Preto'
+
+console.log(corPadrao)
+// Preto
