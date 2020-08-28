fe2c5065a1e166172e72ebb377e682c19604ed3d
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-08-28 02:49:52 -0300

    ago28

---
+++ aula43-ExercicioSwitchEDate/assets/js/mainMeuJeito.js
@@ -0,0 +1,13 @@
+const displayData = document.querySelector('.data')
+const displayHora = document.querySelector('.hora')
+
+
+const data1 =  `<h1>terça-feira, 7 de novembro de 2033</h1>` // dataNaTela ()     
+const hora1 =  '<h1>12:00</h1>'                              // horaNaTela ()     
+
+
+displayData.innerHTML = data1
+displayHora.innerHTML = hora1
+
+ // `${diaSemanaPt}, ${diaMes} de ${mesPt} de ${ano}`
+ // hora
