ce30077214c442531c76fc8c029a19e1452a06fd
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-08-29 02:22:31 -0300

    ago29

--- aula43-ExercicioSwitchEDate/assets/js/test.js
+++
@@ -1,8 +0,0 @@
-const displayData = document.querySelector('.data1')
-const displayHora = document.querySelector('.hora1')
-
-let data1 = `dataNaTela`    
-let hora1 = `horaNaTela`    
-
-displayData.innerHTML =+ data1
-displayHora.innerHTML =+ hora1
