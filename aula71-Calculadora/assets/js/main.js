7b627a56bcea06d278fc768a206145f56c2df25b
Thiago Antunes Milhomens <68217811+ThiagoMilhomens@users.noreply.github.com>
2020-09-14 22:58:06 -0300

    sep 14

--- aula71-Calculadora/assets/js/main.js
+++ aula71-Calculadora/assets/js/main.js
@@ -1,2 +1,39 @@
 
-//alert('o/')
+function criaCalculadora() {
+    // objeto calculadora 
+    return {
+        
+        // CHAVE DISPLAY (linkando os valores do display) 
+        display: document.querySelector('.display'), 
+
+        inicia() {
+            this.cliqueBotoes()
+        },
+        
+        // CAPTURAR CLICK
+        cliqueBotoes() {
+                // this >> escopo 'criarCalculadora()'           
+            document.addEventListener('click', function(e) {
+                // this >> escopo 'document'
+                // JOGANDO VALORES NO DISPLAY 1              
+                const el = e.target
+
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText) // 'this' precisa funcionar somente neste escopo.
+                }
+
+            }.bind(this)); // << fechando 'this' neste escopo com '.bind()' 
+            // se fosse uma arrow function não necessitaria de '.bind()'
+            // arrow function limita automaticamnete o this apenas ao seu escopo
+        },
+
+        // JOGANDO VALORES NO DISPLAY 2  
+        btnParaDisplay(valor) {
+            this.display.value += valor
+
+        }
+    }
+}
+
+const calculadora = criaCalculadora()
+calculadora.inicia()
